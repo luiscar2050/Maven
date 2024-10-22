63c382ec5bb73bfec7ad90959da0411f843efd0d
luiscar2050 <luis3206162549@gmail.com>
2024-10-22 07:25:44 -0500

    clase calculadora

---
+++ src/main/java/org/example/Calculadora.java
@@ -0,0 +1,12 @@
+package com.ejemplo;
+
+public class Calculadora {
+
+    public int sumar(int a, int b) {
+        return a + b;
+    }
+
+    public int restar(int a, int b) {
+        return a - b;
+    }
+}
